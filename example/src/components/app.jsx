65f5f628618d977104577f78c6d3ad0df9658797
Vladimir Osokin <osokin.home@gmail.com>
2021-10-01 16:43:03 +0500

    update example

--- example/src/components/app.jsx
+++ example/src/components/app.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-
 import ExampleComponent from 'mapboxr-gl';
+
+import styles from './app.module.css';
 
 const App = () => {
   console.log(ExampleComponent);
-  return null;
+  return (
+    <div className={styles.wrapper}>
+      <aside className={styles.sidebar}>Controlls</aside>
+      <div className={styles.container}>Map</div>
+    </div>
+  );
 };
 
 export default App;
